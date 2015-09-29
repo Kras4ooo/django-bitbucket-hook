86c821a6c3b14ec1490f96ef4e406bc1ef451d5d
Krasimir Nikolov <krasimir.nikolov@mtr-design.com>
2015-09-29 23:55:58 +0300

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup
+
+
+setup(
+    name='django-bitbucket-hook',
+    version='1.0.0',
+    packages=['django_bitbucket_hook', 'django_bitbucket_hook.migrations'],
+    url='https://github.com/Kras4ooo/django-bitbucket-hook',
+    license='Apache License Version 2.0',
+    author='Krasimir',
+    author_email='krasimir.nikolov1994@gmail.com',
+    classifiers=[
+        'Intended Audience :: Developers',
+        'Framework :: Django :: 1.7',
+        'Framework :: Django :: 1.8',
+        'License :: OSI Approved :: Apache Software License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ],
+    description=''
+)
