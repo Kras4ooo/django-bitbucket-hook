17f329ae2b76a432ebda3b5e502148afb7faba95
Krasimir Nikolov <krasimir.nikolov@mtr-design.com>
2015-10-01 00:02:44 +0300

    Fixed Build

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,1 +1,13 @@
-__author__ = 'krassi'
+import os
+os.environ['DJANGO_SETTINGS_MODULE'] = "tests.settings"
+
+from django.test.utils import setup_test_environment
+setup_test_environment()
+
+# Django 1.7
+import django
+if hasattr(django, 'setup'):
+    django.setup()
+
+from django.core.management import call_command
+call_command('migrate')
